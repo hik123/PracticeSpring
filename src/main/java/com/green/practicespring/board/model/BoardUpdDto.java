18083b9e241d75254aba2f8475fdcef9e7b6f5d5
firstt <khi05040@naver.com>
2023-11-09 18:10:30 +0900

    11-09스프링 연습

---
+++ src/main/java/com/green/practicespring/board/model/BoardUpdDto.java
@@ -0,0 +1,12 @@
+package com.green.practicespring.board.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardUpdDto {
+    private String title;
+    private String ctnts;
+    private String writer;
+}
